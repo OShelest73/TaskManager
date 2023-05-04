70291273830557063002bf4851052dbb21ba0388
OShelest73 <90648269+OShelest73@users.noreply.github.com>
2023-05-04 23:10:19 +0300

    User management pt1

--- TaskManager/Models/UserModel.cs
+++ TaskManager/Models/UserModel.cs
@@ -6,7 +6,12 @@ public class UserModel
 {
     [Key]
     public string EmailAddress { get; set; }
+    [Display(Name = "Full Name")]
+    [Required(ErrorMessage = "Full name is required")]
+    [StringLength(50, MinimumLength = 3, ErrorMessage = "Full name must be between 3 and 50 characters")]
     public string FullName { get; set; }
+    [Display(Name = "Category")]
+    [Required(ErrorMessage = "Category is required")]
     public CategoryModel Category { get; set; }
     public string Password { get; set; }
     public string Salt { get; set; }
